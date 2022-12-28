60e2f2234b8407b328cdd875ebe17a2e011e677d
Michał Ryzner <mryzner@interia.pl>
2022-12-28 14:57:01 +0100

    HelloWorld!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                System.out.println("Michal");
+                System.out.println(42);
+                System.out.println('R');
+    }
+}
