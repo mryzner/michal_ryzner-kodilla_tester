b99a3c23f988e3ac241d376fa5b5da7226bb008b
Michał Ryzner <mryzner@interia.pl>
2022-12-28 20:46:02 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String countryName = "Polska";
+        char initialLetter = 'P';
+        int independenceYear = 1918;
+        String capitalCity = "Warszawa";
+        double populationMlns = 37.99;
+        boolean isAMonarchy = false;
+        System.out.println(countryName);
+        System.out.println(initialLetter);
+        System.out.println(independenceYear);
+        System.out.println(capitalCity);
+        System.out.println(populationMlns);
+        System.out.println(isAMonarchy);
+    }
+}
