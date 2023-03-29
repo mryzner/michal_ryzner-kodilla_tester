0b2d7baf38169aa07e1babb07ebd0818f1a9d1c6
Michał Ryzner <mryzner@interia.pl>
2023-03-29 10:40:00 +0200

    StampsApplication TestSuite

--- kodilla-collections/src/test/java/com/kodilla/collections/sets/homework/StampsApplicationTestSuite.java
+++ kodilla-collections/src/test/java/com/kodilla/collections/sets/homework/StampsApplicationTestSuite.java
@@ -11,16 +11,16 @@ import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 public class StampsApplicationTestSuite {
     Set<Stamp> stamps = new HashSet<>();
     @Test
-    public void shouldNotAddTwoIdenticalStamps() {
+    public void shouldAddStampToTheSet() {
         stamps.add(new Stamp("Ugly stamp", 4, 3, true));
-        stamps.add(new Stamp("Old stamp", 5, 4, false));
-        stamps.add(new Stamp("Nice stamp", 3, 4, true));
-        stamps.add(new Stamp("Old stamp", 5, 4, false));
-        stamps.add(new Stamp("Lovely stamp", 6, 2, false));
-        stamps.add(new Stamp("Valuable stamp", 5, 4, true));
-        stamps.add(new Stamp("Valuable stamp", 5, 4, true));
+        assertEquals(1, stamps.size());
+    }
+    @Test
+    public void shouldNotAddIdenticalStampsToTheSet() {
+        stamps.add(new Stamp("Ugly stamp", 4, 3, true));
+        stamps.add(new Stamp("Ugly stamp", 4, 3, true));
         stamps.add(new Stamp("Ugly stamp", 4, 3, true));
 
-        assertEquals(5, stamps.size());
+        assertEquals(1, stamps.size());
     }
 }
