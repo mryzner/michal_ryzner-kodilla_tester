8d7c6ede70cc441d17492714717e8fbe7e9c2964
Michał Ryzner <mryzner@interia.pl>
2023-04-05 18:02:43 +0200

    5.4 FlightFinder assertTrue

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTest.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTest.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FlightFinderTest {
     FlightFinder flightFinder = new FlightFinder(FlightFinder.flightList);
+    List<Flight> expectedFlights = new ArrayList<>();
     @Test
     public void shouldAddFlightsCorrectly() {
-        List<Flight> expectedFlights = new ArrayList<>();
         List<Flight> result = flightFinder.flightList;
         expectedFlights.add(new Flight("Warsaw", "Berlin"));
         expectedFlights.add(new Flight("Warsaw", "Paris"));
@@ -21,29 +22,35 @@ public class FlightFinderTest {
         expectedFlights.add(new Flight("Tokyo", "London"));
         //expectedFlights.add(new Flight("Warsaw", "Berlin"));
         assertEquals(expectedFlights, result);
+        /*assertTrue(expectedFlights.size() == result.size()
+                && expectedFlights.containsAll(result)
+                && result.containsAll(expectedFlights));*/
     }
     @Test
     public void testFindFlightsFrom() {
-        List<Flight> expectedFlights = new ArrayList<>();
         List<Flight> result = flightFinder.findFlightsFrom("Warsaw");
         expectedFlights.add(new Flight("Warsaw", "Berlin"));
         expectedFlights.add(new Flight("Warsaw", "Paris"));
         //expectedFlights.add(new Flight("Warsaw", "Berlin"));
         assertEquals(expectedFlights, result);
+        /*assertTrue(expectedFlights.size() == result.size()
+                && expectedFlights.containsAll(result)
+                && result.containsAll(expectedFlights));*/
     }
     @Test
     public void testFindFlightsTo() {
-        List<Flight> expectedFlights = new ArrayList<>();
         List<Flight> result = flightFinder.findFlightsTo("Berlin");
         expectedFlights.add(new Flight("Warsaw", "Berlin"));
         expectedFlights.add(new Flight("London", "Berlin"));
         expectedFlights.add(new Flight("New York", "Berlin"));
         //expectedFlights.add(new Flight("Warsaw", "Berlin"));
         assertEquals(expectedFlights, result);
+        /*assertTrue(expectedFlights.size() == result.size()
+                && expectedFlights.containsAll(result)
+                && result.containsAll(expectedFlights));*/
     }
     @Test
     public void testNoSuchFlight() {
-        List<Flight> expectedFlights = new ArrayList<>();
         List<Flight> result1 = flightFinder.findFlightsTo("Szczebrzeszyn");
         List<Flight> result2 = flightFinder.findFlightsFrom("Szczekociny");
         assertEquals(expectedFlights, result1);
@@ -51,7 +58,6 @@ public class FlightFinderTest {
     }
     @Test
     public void testEmptyEntry() {
-        List<Flight> expectedFlights = new ArrayList<>();
         List<Flight> result = flightFinder.findFlightsTo("");
         assertEquals(expectedFlights, result);
     }
