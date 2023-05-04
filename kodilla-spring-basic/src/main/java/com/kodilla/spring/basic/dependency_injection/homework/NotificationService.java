c17ed68c0fb116b31d33d05524d27f56ee499957
Michał Ryzner <mryzner@interia.pl>
2023-05-04 17:08:20 +0200

    9.1 poprawione

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
@@ -1,7 +1,7 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
 public interface NotificationService {
-    public void success(String address);
+    void success(String address);
 
-    public void fail(String address);
+    void fail(String address);
 }
