cae653c1e4ecc60cba1cb490a85fe6cef6e1143b
Michał Ryzner <mryzner@interia.pl>
2023-06-21 14:49:24 +0200

    kodilla-intro

--- kodilla-intro/src/main/java/MyTest3.java
+++
@@ -1,9 +0,0 @@
-public class MyTest3 {
-    public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
-
-        if (2 > 1){
-            System.out.println("Niesamowite! Dwa jest większe niż jeden!!!");
-        }
-    }
-}
--- kodilla-intro/src/main/java/MyTest4.java
+++
@@ -1,9 +0,0 @@
-public class MyTest4 {
-    public static void main(String[] args) {
-        if (1 > 2) {
-            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
-    }
-}
