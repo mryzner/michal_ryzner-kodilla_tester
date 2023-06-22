bf5b4a373d649c1cade67f8b4d6a76406fff1355
Michał Ryzner <mryzner@interia.pl>
2023-06-22 22:52:25 +0200

    kodilla-intro

---
+++ kodilla-intro/src/main/java/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        printHello();
+    }
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+}
