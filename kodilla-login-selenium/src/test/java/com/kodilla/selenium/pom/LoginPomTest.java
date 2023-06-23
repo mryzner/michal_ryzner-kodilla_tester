87abd30c46cb3778c2d3a4d24cb277ba05516215
unknown <mryzner@interia.pl>
2023-06-23 20:20:39 +0200

    kodilla

--- kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/LoginPomTest.java
+++ kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/LoginPomTest.java
@@ -1,10 +1,12 @@
-package com.kodilla.selenium.pom.old;
+package com.kodilla.selenium.pom;
 
+import com.kodilla.selenium.pom.old.LoginPom;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LoginPomTest {
     LoginPom loginPom;
